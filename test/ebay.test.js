ca636b964d0dfe66dfa1c0c75713249b1ded6089
Gary <amezheritskiy@gmail.com>
2024-09-29 22:12:06 -0400

    Adding first set of tests

--- test/ebay.test.js
+++ test/ebay.test.js
@@ -3,22 +3,33 @@ import path from 'path';
 
 async function getItemTitleByIndex(page, index) {
   return await page.evaluate((itemIndex) => {
-    // Adjust for zero-based indexing
     const item = document.querySelector(`.b-list__items_nofooter .s-item:nth-child(${itemIndex + 1})`);
-    
     if (item) {
       const titleElement = item.querySelector('.s-item__title');
-      return titleElement ? titleElement.innerText : null; // Return the title text or null
+      return titleElement ? titleElement.innerText : null;
     }
-    
-    return null; // Return null if the item doesn't exist
-  }, index); // Pass index as an argument to the evaluate function
+    return null;
+  }, index);
+}
+
+async function getChromeExtensionId(page) {
+  await page.goto('chrome://extensions/');
+
+  return await page.evaluate(() => {
+    const firstShadowHost = document.querySelector('extensions-manager');
+    const firstShadowRoot = firstShadowHost.shadowRoot;
+    const secondShadowHost = firstShadowRoot.querySelector('extensions-item-list');
+    const secondShadowRoot = secondShadowHost.shadowRoot;
+
+    const extensionItem = secondShadowRoot.querySelector('extensions-item');
+    return extensionItem ? extensionItem.id : null;
+  });
 }
 
 
-describe('Test extension', () => {
+describe('Test extension in Chrome', () => {
   const timeout = 10000;
-  let browser, page;
+  let browser, page, extensionId;
 
   beforeAll(async () => {
     const extensionPath = path.resolve(__dirname, '../dist');
@@ -34,6 +45,8 @@ describe('Test extension', () => {
     });
 
     page = await browser.newPage();
+
+    extensionId = await getChromeExtensionId(page);
   });
 
   it('should hide item on eBay category page when hide item clicked', async () => {
@@ -41,7 +54,7 @@ describe('Test extension', () => {
 
     // Get the number of items before clicking the hide button, and the name of the second item
     const initialItemCount = await page.evaluate(() => {
-      return document.querySelectorAll('.b-list__items_nofooter .s-item').length; // Count items without nesting
+      return document.querySelectorAll('.b-list__items_nofooter .s-item').length;
     });
     const secondItemTitle = await getItemTitleByIndex(page, 1);
 
@@ -59,6 +72,76 @@ describe('Test extension', () => {
     // Assert that the number of items has decreased by 1, and the title of the first item is the same as the previously second item's title
     expect(currentItemCount).toBe(initialItemCount - 1);
     expect(secondItemTitle).toBe(firstItemTitle);
+  }, timeout);
+
+  it('should hide item on eBay search page when hide item clicked', async () => {
+    await page.goto(`https://www.ebay.com/sch/i.html?_nkw=Acer+Predator+Helios+300')}`);
+
+    // Get the number of items before clicking the hide button, and the name of the second item
+    const initialItemCount = await page.evaluate(() => {
+      return document.querySelectorAll('.srp-results .s-item').length;
+    });
+    const secondItemTitle = await getItemTitleByIndex(page, 1);
+
+    // Click on the first "hide item" button
+    const hideItemButtonSelector = '.hide-item-button';
+    await page.waitForSelector(hideItemButtonSelector);
+    await page.click(hideItemButtonSelector);
+
+    // Get the number of items after clicking the hide button, and the name of the first item
+    const currentItemCount = await page.evaluate(() => {
+        return document.querySelectorAll('.srp-results .s-item').length;
+    });
+    const firstItemTitle = await getItemTitleByIndex(page, 0);
+
+    // Assert that the number of items has decreased by 1, and the title of the first item is the same as the previously second item's title
+    expect(currentItemCount).toBe(initialItemCount - 1);
+    expect(secondItemTitle).toBe(firstItemTitle);
+  }, timeout);
+
+  it('should hide items from a seller blocked in easyBlockStorageObject', async () => {
+    // Get the number of items in the search results and the name of the first seller
+    await page.goto('https://www.ebay.com/sch/i.html?_nkw=bicycle');
+
+    const initialItemCount = await page.evaluate(() => {
+      return document.querySelectorAll('.srp-results .s-item').length;
+    });
+
+    const sellerToBlock = await page.evaluate(() => {
+        const sellerInfoElement = document.querySelector('li .s-item__info .s-item__seller-info-text');
+        return sellerInfoElement ? sellerInfoElement.textContent.trim().toLowerCase() : null;
+    });
+
+    const sellerToBlockName = sellerToBlock.split(" ")[0].toLowerCase();
+
+    // Go to the popup page and block the first seller
+    await page.goto(`chrome-extension://${extensionId}/popup/popup-ebay.html`);
+    await page.click('#nav-sellers-tab');
+    await page.waitForSelector('.userid-input');
+    await page.evaluate((sellerName) => {
+      const inputField = document.querySelector('.userid-input');
+      inputField.value = sellerName;
+      inputField.dispatchEvent(new Event('input', { bubbles: true }));
+    }, sellerToBlockName);
+    await new Promise((r) => setTimeout(r, 500));
+    await page.click('.hide-button');
+
+    // Go back to the search page
+    await page.goto('https://www.ebay.com/sch/i.html?_nkw=bicycle');
+
+    // Get the number of items in the search results and whether any are from the blocked seller
+    const currentItemCount = await page.evaluate(() => {
+      return document.querySelectorAll('.srp-results .s-item').length;
+    });
+
+    const sellerItemsVisible = await page.evaluate((blockedSeller) => {
+        const sellerItems = Array.from(document.querySelectorAll('li .s-item__info .s-item__seller-info-text'));
+        return sellerItems.some(seller => seller.textContent.trim().toLowerCase() === blockedSeller);
+    }, sellerToBlockName);
+
+    // Assert that no items from the blocked seller are visible
+    expect(sellerItemsVisible).toBe(false);
+    expect(currentItemCount).toBeLessThan(initialItemCount);
   }, timeout);
 
   afterAll(async () => {
